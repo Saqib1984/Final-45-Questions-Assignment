80795f7635d88357a3b93a78f12ec1ff9ff1a41a
Saqib1984 <saqibsiddiqui1984@gmail.com>
2024-03-05 21:31:33 +0500

    third commit

---
+++ question-12/q12.js
@@ -0,0 +1,5 @@
+"use strict";
+// Store the names of a few of your friends in a array called names
+let friends_name = ["Ali", "Hamza", "Khawar", "Imran"];
+// . Print each person’s name and message (The text of each message should be the same)
+friends_name.forEach(friend => console.log(`Hello ${friend}, hope you are doing well.`));
