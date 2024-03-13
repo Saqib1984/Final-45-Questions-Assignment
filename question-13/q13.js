0c2da81e3645f2b2a3e52f27e36419ca1dbb6a4a
Saqib1984 <saqibsiddiqui1984@gmail.com>
2024-03-13 19:07:26 +0500

    third commit

---
+++ question-13/q13.js
@@ -0,0 +1,3 @@
+"use strict";
+let transportation_vehicle = ["Honda 125 Bike", "Toyota Corolla", "Honda Civic"];
+transportation_vehicle.forEach(transport_vehicle => console.log(`I would like to own a ${transport_vehicle}`));
---
+++ question-14/q14.js
@@ -0,0 +1,4 @@
+"use strict";
+// Guest List 
+let guest_list = ["Ali Raza", "Raja M Waqas", "Hamza Bhatti"];
+guest_list.forEach(guest => console.log(`Hello ${guest}, today would you like to take dinner with me?`));
